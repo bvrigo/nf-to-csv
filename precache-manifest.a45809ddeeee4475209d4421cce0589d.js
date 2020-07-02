f264e7249e13cbfc35eed88846f5470dca948a54
Bruno Rigo <b.v.rigo@gmail.com>
2020-07-02 19:21:01 -0400

    Updates

---
+++ precache-manifest.a45809ddeeee4475209d4421cce0589d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3166578e4d6c6c7b8b70f49741b2591",
+    "url": "/bvrigo/nf-to-csv/index.html"
+  },
+  {
+    "revision": "1a81b3dd560dd2d53975",
+    "url": "/bvrigo/nf-to-csv/static/css/main.6ea8b906.chunk.css"
+  },
+  {
+    "revision": "eaf4aee9d18c80658717",
+    "url": "/bvrigo/nf-to-csv/static/js/2.e52a9718.chunk.js"
+  },
+  {
+    "revision": "697e367ae7c6281023bee0bece9157a9",
+    "url": "/bvrigo/nf-to-csv/static/js/2.e52a9718.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a81b3dd560dd2d53975",
+    "url": "/bvrigo/nf-to-csv/static/js/main.05eecef2.chunk.js"
+  },
+  {
+    "revision": "f8199d6983d72efa6817",
+    "url": "/bvrigo/nf-to-csv/static/js/runtime-main.f9c78e59.js"
+  }
+]);
